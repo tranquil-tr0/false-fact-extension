ff9c4e0d084e6a8e7b5915143f2a115ef9448d92
tranquil-tr0 <tranquiltr0@proton.me>
2025-07-24 23:46:37 -0700

    rm bad argument

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,8 +3,5 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   manifest: {
     permissions: ['tabs', 'scripting', 'activeTab'],
-    background: {
-      service_worker: 'background.ts', // or 'background.js' if using JS
-    },
   },
 });
