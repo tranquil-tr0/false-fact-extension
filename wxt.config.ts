ceee5b4b32e33014a4e850b55cad16134037adac
tranquil-tr0 <tranquiltr0@proton.me>
2025-07-24 23:41:36 -0700

    im so confused

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   manifest: {
     permissions: ['tabs', 'scripting', 'activeTab'],
+    background: {
+      service_worker: 'background.ts', // or 'background.js' if using JS
+    },
   },
 });
