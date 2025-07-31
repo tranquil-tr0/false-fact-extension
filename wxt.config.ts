648e2a6323a52e14ad19ea76e13693f0c5bb666f
tranquil-tr0 <tranquiltr0@proton.me>
2025-07-30 22:46:44 -0700

    Oops i did the previous commit wrong this one i give it a name finally also it updated the readme (prev commit)

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "wxt";
 
 export default defineConfig({
-  name: 'False Fact'
   modules: ["@wxt-dev/auto-icons"],
   autoIcons: {
     grayscaleOnDevelopment: false,
   },
   manifest: {
+    name: "False Fact",
     permissions: ["scripting", "activeTab", "storage"],
   },
 });
